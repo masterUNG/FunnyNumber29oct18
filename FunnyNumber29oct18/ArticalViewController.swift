84d390f553329bf454ec39c66f4ebf1de3f62295
masterUNG <phrombutr@gmail.com>
2018-10-29 15:41:53 +0700

    Two ViewController

---
+++ FunnyNumber29oct18/ArticalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticalViewController.swift
+//  FunnyNumber29oct18
+//
+//  Created by MasterUNG on 29/10/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ArticalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
