7b15ecf8af6a014d5e6b8ce8f21d8b8b7896f955
masterUNG <phrombutr@gmail.com>
2018-10-30 10:17:32 +0700

    Number Finish

---
+++ FunnyNumber29oct18/MyClass.swift
@@ -0,0 +1,28 @@
+//
+//  MyClass.swift
+//  FunnyNumber29oct18
+//
+//  Created by MasterUNG on 30/10/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import Foundation
+
+class MyClass{
+    var myNumberInt: Int = 0
+    
+    func decreseNum() -> String {
+        myNumberInt -= 1
+        return String(myNumberInt)
+    }
+    
+    func increase() -> String {
+        myNumberInt += 1
+        return String(myNumberInt)
+    }
+    
+    
+    
+}   // MyClass
+
+
--- FunnyNumber29oct18/ViewController.swift
+++ FunnyNumber29oct18/ViewController.swift
@@ -9,10 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    Explicit
+    var currentNumberInt: Int = 0
+    var myClass = MyClass()
+    
+    
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+    @IBAction func decreseButton(_ sender: Any) {
+        
+//        Get Value from Label
+        currentNumberInt = findCurrentNumber()
+        print("currentNumber ==> \(currentNumberInt)")
+        
+//        Assign Value to MyClass
+        myClass.myNumberInt = currentNumberInt
+        print("myNumberInt_myClass ==> \(myClass.myNumberInt)")
+        
+        numberLabel.text = myClass.decreseNum()
+        
+    }
+    
+    @IBAction func increseButton(_ sender: Any) {
+        
+        myClass.myNumberInt = findCurrentNumber()
+        numberLabel.text = myClass.increase()
+        
+    }
+    
+    @IBAction func homeButton(_ sender: Any) {
+        numberLabel.text = "0"
+    }
+    
+    func findCurrentNumber() -> Int {
+        let currentNumber: Int = Int(numberLabel.text!)!
+        return currentNumber
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
 
